c426d1362aad7866f35032f483ab7bb46e159413
zouggari <zouggaritaha6@gmail.com>
2025-03-10 17:56:57 +0100

    adding resources

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -13,14 +13,157 @@
         <div class="w-20 bg-gray-900 p-4 rounded-xl shadow-lg flex flex-col items-center gap-4">
             <p class="text-white text-sm font-semibold">Drag & Drop</p>
             <div class="space-y-4">
-                <img src="https://s.hs-data.com/bilder/spieler/gross/13029.jpg" class="draggable fixed-size-image cursor-pointer" draggable="true" data-url="https://via.placeholder.com/100">
                 <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/book-open-reader.png') }}" 
-     class="draggable fixed-size-image cursor-pointer" 
-     draggable="true" 
-     data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/book-open-reader.png') }}">
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/book-open-reader.png') }}">
+           
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/euro-sign.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/euro-sign.png') }}">
 
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/icto version imprimable.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/icto version imprimable.png') }}">
 
-                <img src="https://via.placeholder.com/100/00ff00" class="draggable fixed-size-image cursor-pointer" draggable="true" data-url="https://via.placeholder.com/100/00ff00">
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/insta.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/insta.png') }}">
+
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/magnifying-glass.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/magnifying-glass.png') }}">
+
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto bars menu burger.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto bars menu burger.png') }}">
+
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto boussole.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto boussole.png') }}">
+
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto calendrier.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto calendrier.png') }}">
+
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto checked.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto checked.png') }}">
+
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto convaincre.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto convaincre.png') }}">
+               
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto dev act lib.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto dev act lib.png') }}">
+               
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto dev act lib2.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto dev act lib2.png') }}">
+               
+
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto dev activité libérale.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto dev activité libérale.png') }}">
+               
+
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto envoi demande.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto envoi demande.png') }}">
+               
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto formation bilan année.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto formation bilan année.png') }}">
+               
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto newsletter.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto newsletter.png') }}">
+               
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto smiley.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto smiley.png') }}">
+               
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto user.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto user.png') }}">
+               
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_elearning_sfp.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_elearning_sfp.png') }}">
+               
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_enveloppe_sfp.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_enveloppe_sfp.png') }}">
+               
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_euro_sfp.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_euro_sfp.png') }}">
+               
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_fb_SFP.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_fb_SFP.png') }}">
+               
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_fleche_sfp.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_fleche_sfp.png') }}">
+               
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_heures_sfp.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_heures_sfp.png') }}">
+               
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_insta_sfp.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_insta_sfp.png') }}">
+               
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_linkedin_sfp.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_linkedin_sfp.png') }}">
+                
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_localisation_sfp.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_localisation_sfp.png') }}">
+
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_loupe.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_loupe.png') }}">
+
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_poignéemain_sfp.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/picto_poignéemain_sfp.png') }}">
+
+                <img src="{{ asset('images/PICTOGRAMMES FONTAWESOME/sablier.png') }}" 
+                class="draggable fixed-size-image cursor-pointer" 
+                draggable="true" 
+                data-url="{{ asset('images/PICTOGRAMMES FONTAWESOME/sablier.png') }}">
             </div>
         </div>
 
